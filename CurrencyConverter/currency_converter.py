0a16eba2809b5516ccd732289fa2658559b1e872
Nikolaj Nielsen <supernikolaj@hotmail.com>
2020-12-04 13:27:56 +0100

    CurrencyConverter

---
+++ CurrencyConverter/currency_converter.py
@@ -0,0 +1,25 @@
+import requests
+import secrets
+from math import floor
+
+
+class CurrencyConverter:
+    @staticmethod
+    def Convert(in_cur: str, out_cur: str, amt: int) -> int:
+        return int(floor(CurrencyConverter.exchange_rate(in_cur, out_cur) * amt))
+
+    @staticmethod
+    def exchange_rate(in_cur: str, out_cur: str) -> float:
+        url = f'https://currency-converter13.p.rapidapi.com/convert'
+        querystring = {"from": in_cur, "to": out_cur, "amount": "1"}
+        headers = {
+            'x-rapidapi-key': secrets.rapidapi_key,
+            'x-rapidapi-host': 'currency-converter13.p.rapidapi.com'
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.json()['amount'])
+        return response.json()['amount']
+
+
+if __name__ == '__main__':
+    print(CurrencyConverter.Convert('USD', 'EUR', 100))
